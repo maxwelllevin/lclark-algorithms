9f6a5000e787f78c5ff4e273168f2a7ae2bc66bf
Maxwell <maxwellevin@users.noreply.github.com>
2017-09-15 12:51:46 -0700

    Add files via upload

--- Falling Sand Lab/src/FallingSandModel.java
+++ Falling Sand Lab/src/FallingSandModel.java
@@ -20,36 +20,36 @@ public class FallingSandModel {
 		// two dimensional array to hold 
 		private int[][] grid;
 
-		// variable for mode
+		// variable for mode - empty, sand, metal, water, etc..
 		private int mode;
 
 		public FallingSandModel() {
+			// Constructor
 			grid = new int[MODEL_SIZE][MODEL_SIZE];
 			mode = METAL;
 		}
 		
 		
 		public int getMode() {
-			// TODO Auto-generated method stub
+			// Returns the mode 
 			return mode;
 		}
 		
 		
 		public void setMode(int set) {
-			// TODO Auto-generated method stub
+			// 
 			mode = set;
 		}
 		
 		
 		public int get(int x, int y) {
-			// TODO Auto-generated method stub
+			// Returns the particle type at a specified location
 			return grid[x][y];
 		}
 
 		
 		public boolean inGrid(int x, int y) {
-			// TODO Auto-generated method stub
-			
+			// Returns true if x,y
 			return x >= 0 && x < MODEL_SIZE && y >= 0 && y < MODEL_SIZE;
 		}
 
@@ -61,7 +61,22 @@ public class FallingSandModel {
 
 
 		public void step() {
-			// TODO Auto-generated method stub
+			// Checks random location in grid and if there is a particle
+			// there, simulates its movement
+			
+			Random rand = new Random();
+			int row = rand.nextInt(MODEL_SIZE);
+			int column = rand.nextInt(MODEL_SIZE);
+			
+			// If there is sand, simulate it
+			if (grid[row][column] == SAND) {
+				sandStep(row, column);
+			}
+			
+			// If there is water, simulate it
+			if (grid[row][column] == WATER) {
+				waterStep(row, column);
+			}
 			
 		}
 
@@ -91,6 +106,7 @@ public class FallingSandModel {
 			// down = 0, left = 1, right = 2
 			Random rand = new Random();
 			int direction = rand.nextInt(3);
+			
 			
 			// Water moves downward
 			if (direction == 0 && inGrid(x, y-1) && grid[x][y-1] == EMPTY) {
